06bec4ad38dc24cc21562ff73853f01496a23c63
Mohid Kittur <mohidkittur3@gmail.com>
2025-12-10 12:48:24 +0530

    palindrome.py

---
+++ palindrome.py
@@ -0,0 +1,10 @@
+import sys
+
+if len(sys.argv) > 1:
+    string = sys.argv[1]
+else:
+    string = input("Enter a string: ")
+if string.lower() == string[::-1].lower():
+    print(f"'{string}' is a palindrome.")
+else:
+    print(f"'{string}' is not a palindrome.")
