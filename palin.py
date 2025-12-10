b27dcd8bec67e55c0279a7a58bbf1a21ad0468e2
Mohid Kittur <mohidkittur3@gmail.com>
2025-12-10 13:37:52 +0530

    Replace user input with fixed string for palindrome check
    Change input method to use a fixed string instead of user input.

--- palin.py
+++ palin.py
@@ -3,7 +3,7 @@ import sys
 if len(sys.argv) > 1:
     string = sys.argv[1]
 else:
-    string = input("Enter a string: ")
+    string = 202323
 
 # Check palindrome (case-insensitive)
 if string.lower() == string[::-1].lower():
